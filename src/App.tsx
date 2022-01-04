b610f37ac7749f43bc990216d6cb4ade9a3e277d
MalcolmMello <malcolmmellolima@gmail.com>
2022-01-04 15:12:53 -0300

    requisição geral

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Post } from './types/Post'
 
 function App() {
+  const [posts, setPosts] = useState<Post[]>([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+	loadPosts()
+  }, [])
+
+  const loadPosts = async () => {
+	  setLoading(true)
+	  let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+	  let json = await response.json()
+	  setLoading(false)
+	  setPosts(json)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+		{loading &&
+			<div>Carregando</div>
+		}
+
+		{!loading && posts.length > 0 &&
+			<>
+				<div>Total posts: {posts.length}</div>
+				<div>
+					{posts.map((item, index) => (
+						<div key={index}>
+							<h4>{item.title}</h4>
+							<small>#{item.id} - Usuário: {item.userId}</small>
+							<p>{item.body}</p>
+						</div>
+  					))}
+				</div>
+			</>
+		}
+
+		{!loading && posts.length === 0 &&
+			<div>Não há posts para exibir</div>
+		}
     </div>
   );
 }
